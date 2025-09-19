c648ba143d7b5c1a45f52c0716cdf28027fbc684
leonardobabinski <60757857+leonardobabinski@users.noreply.github.com>
2025-09-19 15:41:15 -0300

    g

--- src/components/Footer.jsx
+++
@@ -1,7 +0,0 @@
-export default function Footer() {
-  return (
-    <footer className="text-center py-6 text-sm text-gray-500 bg-gray-50">
-      © {new Date().getFullYear()} Leonardo Babinski. Todos os direitos reservados.
-    </footer>
-  );
-}
